635cad0564591dd073e94f525230d8c048c95627
Angela Urrutia A <110882097+byangelina@users.noreply.github.com>
2025-08-11 14:08:11 -0400

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,63 @@
+
+# desarrolle un programa que permita ingresar 10 numeros y despliegue el mayor
+# restriccion: no puede usar listas, funcion max, no chatgpt.
+
+ 
+"""
+num = 0
+
+for i in range (10):
+    numero_ingresado = int(input("Ingrese un número: "))   
+    if num >= numero_ingresado:
+        num += 1
+        break
+
+print(f"el número mayor es: {i} ")
+"""
+
+
+
+
+
+
+
+
+# desarrolle un programa que solicite un numero al usuario y despliegue la siguiente serie
+# ejemplo: el usuario ingresa un "10".
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+
+numero_ingresado = input(int("Ingrese un número: "))
+
+num = 0
+
+for i in range (10):
+    if num >= numero_ingresado:
+        num += 1
+    break
+
+
+# desarrolle un programa que permita usar un numero y despliegue por pantalla si un numero es perfecto 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
