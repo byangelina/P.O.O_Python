3b72b33decf17ff36ec348c8c3da03f93ba8013d
Angelina <110882097+byangelina@users.noreply.github.com>
2025-08-14 14:00:45 -0400

    Add files via upload

---
+++ ejercicios_clase2.py
@@ -0,0 +1,61 @@
+"""print("Programa para seleccionar el número mayor de varios números")
+
+numeromayor = 0
+x = 0
+
+while True:
+    numeroinput = int(input("Ingrese un número( 0 para terminar): "))
+    if numeroinput == 0:
+        break
+    if primer_numero:
+        numeromayor = numeroinput
+        primer_numero = False
+    else:
+        if numeroinput > numeromayor:
+            numeromayor = numeroinput
+
+"""
+
+
+
+"""print("Programa para seleccionar el número mayor de varios números")
+
+numeromayor = 0
+x = 1
+
+while x <= 10:
+    numeroinput = int(input("Ingrese un número( 0 para terminar): ")) # este no tiene break asi que no terminará con 0
+    
+    #if x == 1:
+
+    if x == 1 or
+        numeromayor = numeroinput > numeromayor:
+        numeromayor = numeroinput
+        
+    #else:
+        #if numeroinput > numeromayor:
+            #numeromayor = numeroinput
+
+    x = x + 1
+
+print(f"mayor{numeromayor}")"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
