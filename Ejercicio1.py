68f8cf5602a7f25d017267c7eec9870dcddb3b82
Angelina <110882097+byangelina@users.noreply.github.com>
2025-08-18 12:27:22 -0400

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,11 +1,15 @@
-
-
-
+"""
+Juanito es muy detallista en todo, y sabe que para lavar N kilos de ropa 
+necesita 300 gramos de detergente y 100 gramos de suavizante, hoy llegó a su 
+casa y ya no le queda ropa limpia, ayúdelo construyendo y programa que calcule 
+cuanto detergente y suavizante debe usar en N Kilos de ropa.
+"""
 
 input_kilos = int(input("Ingrese la canidtadad de kilos en ropa que desea lavar: "))
 d = input_kilos * 300
 s = input_kilos * 100
 print(f"\nCantidades que usted necesita:\n- Detergente: {d} gr\n- Suavisante: {s} gr\n")
+
 
 
 
