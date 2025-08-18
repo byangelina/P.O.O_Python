23af53d37777bd322183436a9597f36eef84b54d
Angelina <110882097+byangelina@users.noreply.github.com>
2025-08-18 19:08:56 -0400

    Actualizar ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -5,24 +5,6 @@ y desplegar el mayor
 - sin ciclos, sin listas...
 """
 
-"""num1 = int(input("\nIngrese el primer número: "))
-
-num2 = int(input("\nIngrese el segundo número: "))
-
-num3 = int(input("\nIngrese el tercer número: "))
-
-n1 = num1 > num2 and num3 
-n2 = num2  > num1 and num3
-n3 = num3 > num2 and num1
-
-n_mayor = (n1 or n2 or n3)
-
-print(f"\nEl número mayor es: {n_mayor}")
-
-print("Programa finalizado.")
-"""
-
-
 n1 = int(input("\nIngrese el primer número: "))
 n2 = int(input("Ingrese el segundo número: "))
 n3 = int(input("Ingrese el tercer número: "))
