527f2cadeae48cee121d39f4b4bf2d2566a4fb50
Angelina <110882097+byangelina@users.noreply.github.com>
2025-08-18 12:25:59 -0400

    Ejercicios clase 3

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,47 +1,17 @@
 
-# desarrolle un programa que permita ingresar 10 numeros y despliegue el mayor
-# restriccion: no puede usar listas, funcion max, no chatgpt.
 
- 
-"""
-num = 0
 
-for i in range (10):
-    numero_ingresado = int(input("Ingrese un número: "))   
-    if num >= numero_ingresado:
-        num += 1
-        break
 
-print(f"el número mayor es: {i} ")
-"""
+input_kilos = int(input("Ingrese la canidtadad de kilos en ropa que desea lavar: "))
+d = input_kilos * 300
+s = input_kilos * 100
+print(f"\nCantidades que usted necesita:\n- Detergente: {d} gr\n- Suavisante: {s} gr\n")
 
 
 
 
 
 
-
-
-# desarrolle un programa que solicite un numero al usuario y despliegue la siguiente serie
-# ejemplo: el usuario ingresa un "10".
-# 1
-# 12
-# 123
-# 1234
-# 12345
-
-
-numero_ingresado = input(int("Ingrese un número: "))
-
-num = 0
-
-for i in range (10):
-    if num >= numero_ingresado:
-        num += 1
-    break
-
-
-# desarrolle un programa que permita usar un numero y despliegue por pantalla si un numero es perfecto 
 
 
 
