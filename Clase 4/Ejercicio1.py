40c3c469687d633c4f5db865bb70b38c96f23e6a
Angelina <110882097+byangelina@users.noreply.github.com>
2025-08-25 19:13:33 -0400

    Ejercicio1.py

--- Clase 4/Ejercicio1.py
+++ Clase 4/Ejercicio1.py
@@ -1,40 +1,25 @@
-
-
 # par o impar:
-# Editar este código para que funcione con una función
+# Editar el código comentado para que funcione con una función
 
-# crear una funcion que reciba un argumento com numero entero, el parameto evaluará si es par o impar
-# llamar la funcion en el if
-
+# Crear una funcion que reciba un argumento com numero entero, el parameto evaluará si es par o impar
+# Llamar a la funcion en el if
 
 """
 contador = 0
-
 numero = int(input("\nIngrese un número: "))
-
 flecha = "-->"
 
 for i in range(1,numero+1):
-
     if i % 2 == 0 :
-
         print(i,flecha,"par")
-
     else:
-
         print(i,flecha,"impar")
-
     contador =+ numero
 
 print("\nPrograma finalizado.")
 """
 
-
-
 # mi desarrollo
-
-
-
 def par_impar(numero):
     if numero % 2 == 0:
         return True
@@ -42,7 +27,6 @@ def par_impar(numero):
         return False
     
 input_numero = int(int(input("\nIngrese un número: ")))
-
 if par_impar(input_numero):
     print("El número ingresado es par")
 else:
@@ -50,20 +34,27 @@ else:
 
 
 
-
-# desarrollado por el profe:
-"""def es_par(n):
+# desarrollo profe, con booleanos:
+"""
+def es_par(n):
     if n % 2 == 0:
         return True
     else:
         return False
+
 
 num = int(input("Ing. número :"))
 if es_par(num):
     print("Grabar en base datos")
 else:
     print("Enviar correo al usuario que no es par")
+    
 """
+
+
+
+
+
 
 
 
