3a5dc8313d980d598a31c9f78e6a17686b7816ae
Angelina <110882097+byangelina@users.noreply.github.com>
2025-08-25 18:45:26 -0400

    Ejercicio1.py

--- Clase 3/Ejercicio1.py
+++ Clase 3/Ejercicio1.py
@@ -1,11 +1,13 @@
 
 
+# ejercicio en casa
 
+input_kilos = int(input("Ingrese la cantidad de kilos de ropa que desea lavar: "))
 
-input_kilos = int(input("Ingrese la canidtadad de kilos en ropa que desea lavar: "))
 d = input_kilos * 300
 s = input_kilos * 100
-print(f"\nCantidades que usted necesita:\n- Detergente: {d} gr\n- Suavisante: {s} gr\n")
+
+print(f"\nCantidades que usted necesita:\n- Detergente: {d} gr\n- Suavizante: {s} gr\n")
 
 
 
