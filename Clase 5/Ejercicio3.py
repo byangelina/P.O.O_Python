c81b233b44dcef5e92ee1b5f202648ab96d394cc
Angelina <110882097+byangelina@users.noreply.github.com>
2025-08-25 19:18:34 -0400

    Ejercicio3.py

--- Clase 5/Ejercicio3.py
+++ Clase 5/Ejercicio3.py
@@ -1,8 +1,6 @@
-
 # Definiciones:
 # - Los números perfectos son aquellos que son iguales a la suma de sus propios divisores.
 # - el número primo es el que solo es divisible por 1 y por si mismo (y mayor a 1).
-
 
 # cree una funcion para saber si un número es primo
 # (dentro de la funcion usar return, no print):
@@ -24,74 +22,3 @@ if esPrimo(numero):
     print("primo")
 else:
     print("No es primo")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def esPrimo(n):
-    contarDivisores = 0
-    for x in range(1, n + 1, 1):
-        if n % x == 0:
-            contarDivisores = contarDivisores + 1
-
-    if contarDivisores == 2:
-        return True
-    else:
-        return False
-
-"""
-numero = int(input("Ing. número :"))
-if esPrimo(numero):
-    print("primo")
-else:
-    print("No es primo")"""
-
-
-
-def cantidadPrimos(c):
-    contador = 0    
-    numero = 1       
-    string = ""  
-
-    while contador < c:
-        if esPrimo(numero):
-            string += str(numero) + " "
-            contador += 1
-        numero += 1
-    return string
-
-
-inputCantidadPrimos = int(input("\nIngrese cuántos números primos desea desplegar por pantalla: "))
-
-resultado = cantidadPrimos(inputCantidadPrimos)
-
-print(resultado," \n")
-
-
-
-
