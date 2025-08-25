9a4f4141136b4577f81eccabebd531b0d7aec0ae
Angelina <110882097+byangelina@users.noreply.github.com>
2025-08-25 18:32:08 -0400

    Ejercicio3.py

--- Clase 1/Ejercicio3.py
+++ Clase 1/Ejercicio3.py
@@ -2,3 +2,17 @@
 Desarrolle un programa que permita usar un numero y despliegue por pantalla si un numero es
 perfecto.
 """
+
+# desarrollo
+
+numero = int(input("Ingrese un número: "))
+suma_divisores = 0
+
+for i in range(1, numero): 
+    if numero % i == 0:
+        suma_divisores += i
+
+if suma_divisores == numero:
+    print(f"{numero} es un número perfecto.")
+else:
+    print(f"{numero} no es un número perfecto.")
