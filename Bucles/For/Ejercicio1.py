b4a16c7b47d005933c82a00c32c82ca3bcfe7697
Angelina <110882097+byangelina@users.noreply.github.com>
2025-09-01 11:57:23 -0400

    Add files via upload

---
+++ Bucles/For/Ejercicio1.py
@@ -0,0 +1,41 @@
+
+
+# practicando / mejor dicho, aprendiendo
+
+# cómo funciona un for
+for numero in range(1,11):
+    print(f"estoy imprimiendo {numero}")
+
+
+# lo que imprime: 
+"""
+estoy imprimiendo 1
+estoy imprimiendo 2
+estoy imprimiendo 3
+estoy imprimiendo 4
+estoy imprimiendo 5
+estoy imprimiendo 6
+estoy imprimiendo 7
+estoy imprimiendo 8
+estoy imprimiendo 9
+estoy imprimiendo 10"""
+
+# parte en 1 y termina en 10
+# porque el ultimo numero en range(1,11) no se considera.
+
+
+# uso de parametros en range()
+"""
+for i in range(start, stop, step):
+    
+Código a ejecutar en cada iteración
+
+- start: Número inicial (opcional, por defecto es 0).
+- stop: Número final (no incluido en la iteración).
+- step: Incremento o decremento entre valores (opcional, por defecto es 1).
+
+"""
+
+
+
+
