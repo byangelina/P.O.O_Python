414c6a734d3b2ccba5d4bb217fbc0959baa85430
Angelina <110882097+byangelina@users.noreply.github.com>
2025-09-01 11:59:48 -0400

    Add files via upload

---
+++ Tuplas/Ejercicio1.py
@@ -0,0 +1,13 @@
+
+# Las tuplas no son modificables, no puedes borrar, ni actualizar, te quedas con los datos que fueron definido al inicio.
+
+# asi se escriben las tuplas.
+# nombre_tupla=('dato1','dato2','dato3')
+
+tupla=('uno','dos','tres')
+
+tupla.append('cuatro') # este es el nuevo dato, se usa append para agregarlo al final
+
+print(tupla)
+
+# esto imprime error. porque a las tuplas no se les pueden agregar mas datos.
---
+++ Tuplas/Ejercicio2.py
@@ -0,0 +1,6 @@
+
+# segundo ejemplo de tuplas.
+
+nombres=('ana','sol','sofia','mia') # recordar que ( ) los parantesis son tuplas
+
+print(len(nombres)) # son esto imprimo la cantidad de datos que hay almacenados en la tupla
