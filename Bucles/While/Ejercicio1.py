a89cc09e5ea6f867399afa7471c31c59347d7792
Angelina <110882097+byangelina@users.noreply.github.com>
2025-09-01 11:57:42 -0400

    Add files via upload

---
+++ Bucles/While/Ejercicio1.py
@@ -0,0 +1,18 @@
+# editando el codigo anterior
+"""
+for numero in range(1,11):
+    print(f"\nestoy imprimiendo {numero}")
+
+    pregunta = input("\n¿Quieres imprimir mas numeros? s/n: ")           
+    if pregunta != "s": # si responde algo diferente a S (se considera que respondió n)
+        print("Oki, adios\n")
+        break # asi termina el ciclo
+    else: # sino, vuelve a preguntar 
+        print(pregunta)
+
+    # el ciclo se repite hasta llegar a 10
+    # pero, y si quiero que sea ilimitado?, en ese caso debería usar un while true :)
+"""
+
+print("olaa")
+#while True:
